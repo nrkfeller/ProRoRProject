17808bcc860819d9676a818d5babdfa13ba7ccd0
nrkfeller <nrkfeller@gmail.com>
2015-06-18 21:05:37 +0000

    Added partials form, errors, title

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,46 @@
+class CoursesController < ApplicationController
+  
+  def index
+    @courses = Course.all
+  end
+  
+  def show
+    @course = Course.find(params[:id])
+  end
+  
+  def new
+    @course = Course.new
+  end
+  def create
+    @course = Course.new(course_params)
+    @course.user = User.find(2)
+    
+    if @course.save
+      flash[:success] = "Course Created!"
+      redirect_to courses_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @course = Course.find(params[:id])
+  end
+  
+  def update
+    @course = Course.find(params[:id])
+    if @course.update(course_params)
+      flash[:success] = 'Updated Successfully!'
+      redirect_to course_path(@course)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+    def course_params
+      params.require(:course).permit(:name, :rating, :description)
+    end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   get '/home', to: 'pages#home'
+  
+  #get '/courses', to: 'courses#index'
+  #get '/courses/new', to: 'courses#new', as: 'new_course'
+  #post '/courses', to: 'courses#create'
+  #get '/courses/:id/edit', to: 'courses#edit', as: 'edit_course'
+  #patch '/courses/:id', to: 'courses#update'
+  #get '/courses/:id', to: 'courses#show', as: 'course'
+  #delete '/courses/:id', to: 'courses#destroy'
+  
+  resources :courses
+  
 
 
 end
