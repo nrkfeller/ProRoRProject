bfd4c0549161c1be2f6c23acf412cca9d595d8d3
nrkfeller <nrkfeller@gmail.com>
2015-06-21 08:19:52 +0000

    Added navigation paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   
+  def index
+    @users = User.paginate(page: params[:page], per_page: 3)
+  end
+  
+
   def new
     @user = User.new
   end
@@ -30,6 +35,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @courses = @user.courses.paginate(page: params[:page], per_page: 3)
   end
   
   
